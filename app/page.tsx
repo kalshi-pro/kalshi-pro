0d043ad357816f44d66bc462f38e988554138276
Xipu Li <lixipu98@gmail.com>
2025-01-26 23:04:42 -0800

    fix: layout for clerk auth

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
             more.
           </p>
           <div className="mt-6 flex w-full flex-col items-center justify-center gap-4 sm:flex-row">
-            <Link href="/sign-in">
+            <Link href="/sign-up">
               <button className="rounded-lg bg-green-600 px-4 py-2 font-bold text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset] transition duration-200 hover:bg-green-700">
                 Get Started
               </button>
